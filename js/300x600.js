0c51bc5d671a2d0973ff96fd7f346af1dcd4c8e3
sprtrpr <torres.shawn@gmail.com>
2021-07-22 15:47:41 -0400

    Initial commit

---
+++ js/300x600.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+	// Definitions
+  
+	// Functions
+
+  // Animations
+  TweenLite.to(header, .5, { opacity: 1, delay: .7 }, 0);
+  // TweenLite.to(bg1, 1, {  opacity: 0,  delay: .7 }, 0);
+
+
+}
+//end
---
+++ js/turkeyWrap.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+
+	// Definitions
+  
+	// Functions
+
+  // Animations
+  TweenLite.to(hdln, 1, { opacity: 1, delay: .5 }, 0);
+  TweenLite.to(hdln, 1, { opacity: 0, delay: 2 }, 0);
+  TweenLite.to(turkeyWrap, 1, { left: 0, top: 0, rotation: 0, index:2, delay: 2.5 }, 0);
+  TweenLite.to(header, 2, { opacity: 1, delay: 3 }, 0);
+  // TweenLite.to(bg1, 1, {  opacity: 0,  delay: .7 }, 0);
+
+
+}
+//end
